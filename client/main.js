9adbc5c0151b6f48c25e4e348031b78f1fdddc80
Rokas <rookas.rudzenskas@gmail.com>
2022-12-23 15:27:44 +0200

    Add bot and user icons
    This commit adds the bot and user icons to the chat application.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
