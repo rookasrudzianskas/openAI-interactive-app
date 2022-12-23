db871d6635789dab20b6a096ce9c6f1e455bfc41
Rokas <rookas.rudzenskas@gmail.com>
2022-12-23 16:04:52 +0200

    Add server.js
    This is the first commit to the server.js file.

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    // console.log('Hello World!');
+});
+
+app.post('/', async (req, res) => {
+
+});
